e60ade4e3b03cdb19d07a8be3338758bc799c2a5
Patrick McGowan <patrick.mcgowan@acrontum.de>
2021-01-23 13:23:30 +0100

    colors lib modifies string prototype - that's bad. rearrange main method to call more descriptive methods

--- src/generateIt.ts
+++ src/generateIt.ts
@@ -1,74 +1,143 @@
-import 'colors';
-import * as fs from 'fs-extra';
-import path from 'path';
-import globalHelpers from '@/lib/helpers/globalHelpers';
-import { Config } from '@/interfaces/Config';
+import { Config, ConfigExtendedBase } from '@/interfaces';
 import ConfigMerger from '@/lib/ConfigMerger';
 import FileIterator from '@/lib/FileIterator';
 import GeneratedComparison from '@/lib/generate/GeneratedComparison';
 import generateDirectoryStructure from '@/lib/generate/generateDirectoryStructure';
-import TemplateFetch from '@/lib/template/TemplateFetch';
-import OpenAPIBundler from '@/lib/openapi/OpenAPIBundler';
-import logTimeDiff from '@/lib/helpers/logTimeDiff';
 import checkRcOpIdArrIsValid from '@/lib/helpers/checkRcOpIdArrIsValid';
+import globalHelpers from '@/lib/helpers/globalHelpers';
+import OpenAPIBundler from '@/lib/openapi/OpenAPIBundler';
+import TemplateFetch from '@/lib/template/TemplateFetch';
+import * as fs from 'fs-extra';
+import path from 'path';
+
+const bold = (s: string) => `\x1b[1;1m${s}\x1b[0;0m`;
+const green = (s: string, isBold = false) => `\x1b[${Number(isBold)};32m${s}\x1b[0;0m`;
+let now: number;
+
+export const logTimeDiff = (start = now, end = Date.now()) => {
+  console.log(green(`Seconds passed: ${((end - start) / 1000).toFixed(2)}`));
+  start = end;
+};
 
 /**
- * Generates a code skeleton for an API given an OpenAPI/Swagger file.
- * @return {Promise}
+ * Fetches templates.
+ *
+ * @param  {Config}                       config  The configuration
+ *
+ * @return {Promise<ConfigExtendedBase>}  Merged config settings
  */
-export default async (config: Config): Promise<boolean> => {
-  logTimeDiff(0, 0);
-  const startTime = new Date().getTime();
-  globalHelpers(config.verbose, config.veryVerbose);
+const fetchTemplates = async (config: Config): Promise<ConfigExtendedBase> => {
+  console.log(green('Fetching templates...', true));
 
-  console.log('Fetching templates...'.green.bold);
-    const templatesDir = await TemplateFetch.resolveTemplateType(config.template, config.targetDir, config.dontUpdateTplCache);
-    let extendedConfig = await ConfigMerger.base(config, templatesDir);
-  logTimeDiff(startTime, (new Date()).getTime(), true);
+  const templatesDir = await TemplateFetch.resolveTemplateType(
+    config.template,
+    config.targetDir,
+    config.dontUpdateTplCache
+  );
+  const extendedConfig = await ConfigMerger.base(config, templatesDir);
+  logTimeDiff();
 
-  console.log('Preparing openapi object...'.green.bold);
-    const apiObject = await OpenAPIBundler.bundle(config.swaggerFilePath, extendedConfig);
-  logTimeDiff(startTime, (new Date()).getTime(), true);
+  return extendedConfig;
+};
 
-  console.log(`Printing full object to the .openapi-nodegen directory`.green.bold);
-    const baseCompiledObjectPath = path.join(GeneratedComparison.getCacheBaseDir(config.targetDir), 'apiObject.json');
-    fs.ensureFileSync(baseCompiledObjectPath);
-    fs.writeJsonSync(baseCompiledObjectPath, apiObject, {spaces: 2});
-    extendedConfig = ConfigMerger.injectSwagger(extendedConfig, apiObject);
-    checkRcOpIdArrIsValid(extendedConfig.swagger, extendedConfig.nodegenRc);
-  logTimeDiff(startTime, (new Date()).getTime(), true);
+/**
+ * Creates an api object.
+ *
+ * @param  {Config}                       config          The configuration
+ * @param  {ConfigExtendedBase}           extendedConfig  The extended configuration
+ *
+ * @return {Promise<ConfigExtendedBase>}  Merged config settings
+ */
+const createApiObject = async (config: Config, extendedConfig: ConfigExtendedBase): Promise<ConfigExtendedBase> => {
+  console.log(green('Preparing openapi object...', true));
 
-  console.log('Injecting content to files...'.green.bold);
-    await FileIterator.walk(generateDirectoryStructure(extendedConfig, templatesDir), extendedConfig);
-  logTimeDiff(startTime, (new Date()).getTime(), true);
+  const apiObject = await OpenAPIBundler.bundle(config.swaggerFilePath, extendedConfig);
+  logTimeDiff();
 
-  if (!config.dontRunComparisonTool) {
-    console.log('Building stub file comparison list...'.green.bold);
-      const diffObject = await GeneratedComparison.fileDiffs(config.targetDir);
-      await GeneratedComparison.fileDiffsPrint(config.targetDir, diffObject);
-    logTimeDiff(startTime, (new Date()).getTime(), true);
+  console.log(green('Printing full object to the .openapi-nodegen directory', true));
 
-    console.log('Comparison version cleanup...'.green.bold);
-      GeneratedComparison.versionCleanup(config.targetDir);
-    logTimeDiff(startTime, (new Date()).getTime(), true);
+  const baseCompiledObjectPath = path.join(GeneratedComparison.getCacheBaseDir(config.targetDir), 'apiObject.json');
+  fs.ensureFileSync(baseCompiledObjectPath);
+  fs.writeJsonSync(baseCompiledObjectPath, apiObject, { spaces: 2 });
+  extendedConfig = ConfigMerger.injectSwagger(extendedConfig, apiObject);
+  checkRcOpIdArrIsValid(extendedConfig.swagger, extendedConfig.nodegenRc);
+
+  logTimeDiff();
+
+  return extendedConfig;
+};
+
+/**
+ * Iterates files and renders content.
+ *
+ * @param  {ConfigExtendedBase}  extendedConfig  The extended configuration
+ * @param  {string}              templatesDir    The templates dir
+ */
+const injectContent = async (extendedConfig: ConfigExtendedBase, templatesDir: string): Promise<void> => {
+  console.log(green('Injecting content to files...', true));
+
+  await FileIterator.walk(generateDirectoryStructure(extendedConfig, templatesDir), extendedConfig);
+  logTimeDiff();
+};
+
+/**
+ * Generates stub diffs.
+ *
+ * @param  {Config}         config  The configuration
+ */
+const runComparisonTool = async (config: Config): Promise<void> => {
+  if (config.dontRunComparisonTool) {
+    return;
   }
 
+  console.log(green('Building stub file comparison list...', true));
+
+  const startTime = new Date().getTime();
+  const diffObject = await GeneratedComparison.fileDiffs(config.targetDir);
+  await GeneratedComparison.fileDiffsPrint(config.targetDir, diffObject);
+  logTimeDiff();
+
+  console.log(green('Comparison version cleanup...', true));
+  GeneratedComparison.versionCleanup(config.targetDir);
+  logTimeDiff();
+};
+
+/**
+ * Prints the changelog.
+ *
+ * @param {Config}  config  The configuration
+ */
+const printChangelog = (config: Config): void => {
   const changelogFilePath = path.join(config.targetDir, 'changelog.generate-it.json');
+
   if (fs.existsSync(changelogFilePath)) {
     const changelog = fs.readJsonSync(changelogFilePath);
     const latest = changelog.releases.pop();
-    console.log('  ');
-    console.log('  ');
-    console.log('Template version: '.green + latest.templateVersion.green.bold);
-    console.log('Minimum generate-it core version: ' + latest.minCoreVersion.bold);
-    console.log('Tpl Change Description: ' + latest.description.bold);
-    console.log('  ');
-    console.log('  ');
-    console.log('Complete'.green.bold);
-    console.log('  ');
-    console.log('See the changelog for details: '.green.bold + changelogFilePath.green);
-  } else {
-    console.log('Complete'.green.bold);
+
+    console.log(`
+
+${green('Template version:                ')} ${green(latest.templateVersion, true)}
+Minimum generate-it core version: ${bold(latest.minCoreVersion)}
+Tpl Change Description:           ${bold(latest.description)}
+
+${green('See the changelog for details:', true)} ${green(changelogFilePath)}
+`);
   }
+};
+
+export default async (config: Config): Promise<boolean> => {
+  now = Date.now();
+  globalHelpers(config.verbose, config.veryVerbose);
+
+  const extendedConfig = await fetchTemplates(config);
+
+  const apiObject = await createApiObject(config, extendedConfig);
+
+  await injectContent(extendedConfig, extendedConfig.templates);
+
+  await runComparisonTool(config);
+
+  printChangelog(config);
+
   return true;
 };
